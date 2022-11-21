4dee70a8a7232e5fd4748340f1ba396019b7a91f
d3ng4 <denggi2000@gmail.com>
2022-11-21 08:12:29 -0800

    starce

---
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+#usr/bin/env bash
+#replacing right php config files to load php
+exec {'replacing_right path':
+  command => 'sed -i "s/.phpp/.php/g"  /var/www/html/wp-settings.php',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  onlyif  => 'test -f /var/www/html/wp-settings.php'
+}
